3c572392c62f90e3e9b2ed2851d1aa526600f820
Agata Bobrowska <agata.m.bobrowska@gmail.com>
2024-10-21 20:30:12 +0200

    git message

---
+++ src/test/java/pl/akademiaqa/FirstTest.java
@@ -0,0 +1,20 @@
+package pl.akademiaqa;
+        import com.microsoft.playwright.Browser;
+        import com.microsoft.playwright.BrowserType;
+        import com.microsoft.playwright.Page;
+        import com.microsoft.playwright.Playwright;
+        import org.assertj.core.api.Assertions;
+        import org.junit.jupiter.api.Test;
+
+        class FirstTest {       //public class (class available in the package
+
+@Test
+void should_return_correct_page_title() {
+    Playwright playwright = Playwright.create();
+    Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+    Page page = browser.newPage();
+
+    page.navigate("https://playwright.dev/");
+    Assertions.assertThat(page.title()).isEqualTo("Fast and reliable end-to-end testing for modern web apps | Playwright");
+    }
+}
