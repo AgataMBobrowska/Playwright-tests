e89d5dd118de38a9555a179cf87df4c0c8ff1d58
Agata Bobrowska <agata.m.bobrowska@gmail.com>
2024-10-22 16:28:57 +0200

    code changes

--- src/test/java/gabowooltest/AllBrowsersTestGaboWool.java
+++ src/test/java/gabowooltest/AllBrowsersTestGaboWool.java
@@ -1,23 +1,21 @@
-package pl.akademiaqa;
+package gabowooltest;
 
 import com.microsoft.playwright.Page;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Paths;
 
- class AllBrowserTest extends BaseTest{         //public class
+public class AllBrowsersTestGaboWool extends BaseTestGaboWool {
 
     @Test
-    void should_open_supported_browsers_chromium() {
-
-        page.navigate("https://www.whatismybrowser.com/");
+    void should_open_all_browsers_chromium() {
+        page.navigate("https://www.gabowool.pl/");
         page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshots/chromium.png")));
     }
 
     @Test
-    void should_open_supported_browsers_firefox() {
-
-        page.navigate("https://www.whatismybrowser.com/");
+    void should_open_all_bowsers_firefox() {
+        page.navigate("https://www.gabowool.pl/");
         page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshots/firefox.png")));
     }
 }
