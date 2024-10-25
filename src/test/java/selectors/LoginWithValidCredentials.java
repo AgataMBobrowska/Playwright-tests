54b28c96fbdffbcd1672e14059283339655226e6
Agata Bobrowska <agata.m.bobrowska@gmail.com>
2024-10-25 17:38:36 +0200

    Login with valid credentials - update

--- src/test/java/selectors/LoginWithValidCredentials.java
+++ src/test/java/selectors/LoginWithValidCredentials.java
@@ -1,19 +1,22 @@
 package selectors;
 
-import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
 import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
 import common.BaseTest;
 import org.junit.jupiter.api.Test;
 
-public class LoginWithValidCredentials extends BaseTest {
+ class LoginWithValidCredentials extends BaseTest {                         //public class
 
     @Test
     void should_login_with_valid_credentials() {
 
         page.navigate("https://the-internet.herokuapp.com/login");
-        page.fill("#username","tomsmith");                      // # = id
-        page.fill("#password", "SuperSecretPassword!");         // # = id
-        page.click("#login button");
-        PlaywrightAssertions.assertThat(page.locator("text=You logged into a secure area!")).isVisible();       //assertions - build-in wait
+        page.getByLabel("Username").fill("tomsmith");
+        page.getByLabel("Password").fill("SuperSecretPassword!");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+        PlaywrightAssertions.assertThat(page.getByText("You logged into a secure area!")).isVisible();
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Logout")).click();
+        PlaywrightAssertions.assertThat(page.getByText("You logged out of the secure area!")).isVisible();
     }
 }
