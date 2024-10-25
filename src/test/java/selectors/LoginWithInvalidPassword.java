a0d0b7d78edafaa63e08b12ebdb01e2fc6bf0163
Agata Bobrowska <agata.m.bobrowska@gmail.com>
2024-10-25 19:13:08 +0200

    code updates - Login with Invalid Credentials

---
+++ src/test/java/selectors/LoginWithInvalidPassword.java
@@ -0,0 +1,21 @@
+package selectors;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
+import common.BaseTest;
+import org.junit.jupiter.api.Test;
+
+public class LoginWithInvalidPassword extends BaseTest {
+
+    @Test
+    void should_not_login_with_invalid_password() {
+        page.navigate("https://the-internet.herokuapp.com/login");
+        page.getByLabel("Username").click();
+        page.getByLabel("Username").fill("tomsmith");
+        page.getByLabel("Password").click();
+        page.getByLabel("Password").fill("Password!");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName(" Login")).click();
+        PlaywrightAssertions.assertThat(page.getByText("Your password is invalid!")).isVisible();
+    }
+}
