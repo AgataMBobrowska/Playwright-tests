bc2c25612be0bedea1d66f36933384ddfa68862c
Agata Bobrowska <agata.m.bobrowska@gmail.com>
2024-10-28 16:50:51 +0100

    basic actions learn

--- src/test/java/basicpwactionslearn/pageTest.java
+++ src/test/java/basicpwactionslearn/pageTest.java
@@ -1,18 +1,4 @@
-package basicpwactionslearn;
-
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.WaitUntilState;
-import common.BaseTest;
-import org.junit.jupiter.api.Test;
-
-public class pageTest extends BaseTest {
-
-    @Test
-    void pageTest() {
-        page.navigate("https://the-internet.herokuapp.com/login");
-        page.navigate("https://the-internet.herokuapp.com/login", new Page.NavigateOptions().setTimeout(3000)); //default 30 sec.
-        page.navigate("https://the-internet.herokuapp.com/login", new Page.NavigateOptions().setWaitUntil(WaitUntilState.LOAD));
-
+//
         // 1.DOMCONTENTLOADED - html is loaded, but css and js is not
         // 2. LOAD - html, css and js are loaded (DEFAULT)
         // 3. NETWORKIDLE - no additional requests in 500 ms
@@ -28,5 +14,5 @@ public class pageTest extends BaseTest {
 //        page.dblclick("");
 //        page.check("");
 //        page.uncheck("");
-    }
-}
+//    }
+//}
